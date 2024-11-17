0b5ea50e0b79f1060715becf1425c1cd8a1ddda5
Glitch-guy0 <prajwalm9845@gmail.com>
2024-11-17 09:31:14 +0530

    day 2 completed 1video

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# React + Vite
+# Thirtysix Studio-clone (creative development)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a clone of the Thirtysix Studio website, built using Vite, Canvas, and GSAP.
 
-Currently, two official plugins are available:
+The website uses a combination of HTML5 Canvas and GSAP (Greensock Animation Platform) to create a visually striking and interactive experience. The website is fully responsive and works well on both desktop and mobile devices.
+tutorial by **Sheryiens Coding school**
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Run locally
+
+1. Clone this repository
+2. `npm install`
+3. `npm run dev`
+4. Open your browser at [http://localhost:5173](http://localhost:5173)
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -4,33 +4,60 @@ import gsap from "gsap";
 
 import canvasImages from "./canvasimages";
 
-export default function Canvas({ startIndex }) {
+export default function Canvas({ details }) {
+  // destructure the details object
+  const { startIndex, numImages, duration, size, top, left, zIndex } = details;
+
   const canvasRef = useRef(null);
   const [index, setIndex] = useState({ value: startIndex });
 
-  useGSAP(
-    () =>
-      gsap.to(index, {
-        value: startIndex+149,
-        repeat: -1,
-        duration: 3,
-        ease: "linear",
-        onUpdate: () => setIndex({ value: Math.round(index.value) }),
-      })
-  );
+  useGSAP(() => {
+    gsap.to(index, {
+      value: startIndex + numImages - 1,
+      repeat: duration,
+      duration: 3,
+      ease: "linear",
+      onUpdate: () => setIndex({ value: Math.round(index.value) }),
+    });
+
+    gsap.from(canvasRef.current, {
+      opacity: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  });
 
   useEffect(() => {
+    const scale = window.devicePixelRatio;
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
     const image = new Image();
     image.src = canvasImages[index.value];
     image.onload = () => {
-      canvas.width = image.width;
-      canvas.height = image.height;
-      context.drawImage(image, 0, 0);
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+
+      context.scale(scale, scale);
+      context.drawImage(image, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
     };
   }, [index]);
 
-  return <canvas ref={canvasRef} id="canvas" className="size-[22rem]"></canvas>;
+  return (
+    <canvas
+      data-scroll
+      data-scroll-speed={Math.random().toFixed(1)}
+      ref={canvasRef}
+      className="absolute"
+      style={{
+        width: `${size * 1.8}px`,
+        height: `${size * 1.8}px`,
+        top: `${top}%`,
+        left: `${left}%`,
+        zIndex: `${zIndex}`,
+      }}
+      id="canvas"
+    ></canvas>
+  );
 }
-
